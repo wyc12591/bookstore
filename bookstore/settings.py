38521865fffa1bd12cccc935cbd139b08dcd8edb
Duoduo <wyc12591@163.com>
2021-05-19 08:49:38 +0800

    渲染注册页

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -67,6 +67,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')  # 调试时使用的静态文件目录
 ]
 
 WSGI_APPLICATION = 'bookstore.wsgi.application'
---
+++ users/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/19 8:38 上午
+# @Author  : Shark
+# @Site    : 
+# @File    : urls.py
+# @Software: PyCharm
+from django.urls import path
+
+from users import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),  # 用户注册
+]
